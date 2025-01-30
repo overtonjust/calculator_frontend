1760a1ae32b28832a9e8fdd9a3586a76adcc854b
overtonjust <joverton@pursuit.org>
2025-01-30 16:01:23 -0500

    feat: setup operand validation

--- src/pages/home/features/Calculator.tsx
+++ src/pages/home/features/Calculator.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import { FaBackspace } from 'react-icons/fa';
 
 // Modules
-import { clearDisplay, updateDisplay } from '../modules/calcLogic';
+import { clearDisplay, updateDisplay, validateOperands } from '../modules/calcLogic';
 
 const Calculator = () => {
     const [display, setDisplay] = useState('');
@@ -15,27 +15,27 @@ const Calculator = () => {
             <section data-testid='calcScreen' className='calc__screen'>{display}</section>
             <section className='calc__row'>
                 <button onClick={() => clearDisplay(setDisplay)} className='calc__option-btn'>AC</button>
-                <button className='calc__option-btn'>()</button>
-                <button className='calc__option-btn'>%</button>
-                <button value={"÷"} className='calc__option-btn'>÷</button>
+                {/* <button className='calc__option-btn'>()</button> */}
+                {/* <button className='calc__option-btn'>%</button> */}
+                <button value={"÷"} onClick={(e) => validateOperands(e, display, setDisplay)} className='calc__option-btn'>÷</button>
             </section>
             <section className='calc__row'>
                 <button value={7} onClick={(e) => updateDisplay(e, setDisplay)} className='calc__btn'>7</button>
                 <button value={8} onClick={(e) => updateDisplay(e, setDisplay)} className='calc__btn'>8</button>
                 <button value={9} onClick={(e) => updateDisplay(e, setDisplay)} className='calc__btn'>9</button>
-                <button value={"x"} onClick={(e) => updateDisplay(e, setDisplay)} className='calc__option-btn'>x</button>
+                <button value={"x"} onClick={(e) => validateOperands(e, display, setDisplay)} className='calc__option-btn'>x</button>
             </section>
             <section className='calc__row'>
                 <button value={4} onClick={(e) => updateDisplay(e, setDisplay)} className='calc__btn'>4</button>
                 <button value={5} onClick={(e) => updateDisplay(e, setDisplay)} className='calc__btn'>5</button>
                 <button value={6} onClick={(e) => updateDisplay(e, setDisplay)} className='calc__btn'>6</button>
-                <button value={"-"} onClick={(e) => updateDisplay(e, setDisplay)} className='calc__option-btn'>-</button>
+                <button value={"-"} onClick={(e) => validateOperands(e, display, setDisplay)} className='calc__option-btn'>-</button>
             </section>
             <section className='calc__row'>
                 <button value={1} onClick={(e) => updateDisplay(e, setDisplay)} className='calc__btn'>1</button>
                 <button value={2} onClick={(e) => updateDisplay(e, setDisplay)} className='calc__btn'>2</button>
                 <button value={3} onClick={(e) => updateDisplay(e, setDisplay)} className='calc__btn'>3</button>
-                <button value={"+"} onClick={(e) => updateDisplay(e, setDisplay)} className='calc__option-btn'>+</button>
+                <button value={"+"} onClick={(e) => validateOperands(e, display, setDisplay)} className='calc__option-btn'>+</button>
             </section>
             <section className='calc__row'>
                 <button value={0} onClick={(e) => updateDisplay(e, setDisplay)} className='calc__btn'>0</button>
--- src/pages/home/modules/calcLogic.ts
+++ src/pages/home/modules/calcLogic.ts
@@ -1,3 +1,26 @@
+const validateOperands = (
+    event: React.MouseEvent<HTMLButtonElement>,
+    display: string,
+    setDisplay: React.Dispatch<React.SetStateAction<string>>
+) => {
+
+    if(display.length === 0) return;
+
+    const nums = /[0-9]/g;
+    const lastChar = display[display.length -1];
+
+    if(!lastChar.match(nums) && lastChar !== '.') {
+        setDisplay(prev => {
+            const tempArr = prev.split(''); 
+            const target = event.target as HTMLButtonElement
+            tempArr[tempArr.length - 1] = target.value
+
+            return tempArr.join('')
+        })
+    } else {
+        updateDisplay(event, setDisplay);
+    }
+}
 
 const updateDisplay =  (
     event: React.MouseEvent<HTMLButtonElement>, 
@@ -15,4 +38,4 @@ const clearDisplay = (
 };
 
 
-export { updateDisplay, clearDisplay}
+export { updateDisplay, clearDisplay, validateOperands}
